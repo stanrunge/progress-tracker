3a96235443a74d51f9e227e807801080ed37d112
Stan Runge <stan@stanrunge.dev>
2024-10-22 13:23:57 +0200

    lol changes

--- resources/js/Pages/Index.svelte
+++ resources/js/Pages/Index.svelte
@@ -1,10 +1,61 @@
-<script>
+<script lang="ts">
+    import { useForm, Link } from "@inertiajs/svelte";
+
+    let form = useForm({
+        type: "book",
+        name: "",
+        progress: "",
+    });
+
+    function handleSubmit() {
+        $form.post("/tasks");
+    }
+
     export let tasks;
+    export let auth;
+
+    console.log(auth);
 </script>
 
-<h1 class="text-3xl font-bold text-center my-8">
-    Progress Tracker ({tasks.length} tasks)
-</h1>
+<div class="flex justify-around">
+    <div></div>
+
+    <h1 class="text-3xl font-bold text-center my-8">
+        Progress Tracker ({tasks.length} tasks)
+    </h1>
+
+    <div class="content-center">
+        {#if auth.user}
+            <div
+                class="flex items
+            -center"
+            >
+                <img
+                    src={auth.user.profile_photo_url}
+                    alt="Profile Photo"
+                    class="rounded-full h-10 w-10"
+                />
+                <span class="ml-2">{auth.user.name}</span>
+            </div>
+        {:else}
+            <Link href="/login">
+                <button
+                    class="bg-blue-500 rounded py-2 px-4 text-white font-bold"
+                >
+                    Log In
+                </button>
+            </Link>
+
+            <Link href="/register">
+                <button
+                    class="border-2 border-black rounded py-2 px-4 font-bold"
+                >
+                    Register
+                </button>
+            </Link>
+        {/if}
+    </div>
+</div>
 
 <div class="flex justify-center">
     <table>
@@ -19,19 +70,19 @@
         <tbody>
             <tr>
                 <th>
-                    <select name="type" id="">
+                    <select bind:value={$form.type}>
                         <option value="book">Book</option>
                         <option value="course">Course</option>
                         <option value="project">Project</option>
                     </select>
                 </th>
-                <th><input name="name" type="text" /></th>
-                <th><input name="progress" type="text" /></th>
+                <th><input bind:value={$form.name} type="text" /></th>
+                <th><input bind:value={$form.progress} type="text" /></th>
                 <th
                     ><button
                         class="bg-black hover:bg-gray-800 text-white font-bold py-2 px-4 rounded"
                         on:click={() => {
-                            console.log("Save");
+                            handleSubmit();
                         }}>Save</button
                     ></th
                 >
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,5 @@
 
 use App\Http\Controllers\TaskController;
 use Illuminate\Support\Facades\Route;
-use Inertia\Inertia;
 
-Route::get('/', [TaskController::class, 'index']);
+Route::resource('tasks', TaskController::class)->only(['index', 'store']);
