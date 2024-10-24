4fa01aec12da4c270e870ac1a5a2888bb494b25c
Stan Runge <stan@stanrunge.dev>
2024-10-24 19:35:34 +0200

    get tasks records

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -5,22 +5,22 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Inertia\Inertia;
 use Inertia\Response;
 
 class TaskController extends Controller
 {
-    public function index(User $user): Response
+    public function index(): Response
     {
         return Inertia::render('Tasks', [
-            'tasks' => $user->tasks,
+            'tasks' => Auth::user()->tasks,
         ]);
     }
 
     public function store(Request $request): RedirectResponse
     {
         $validated = $request->validate([
-
             'name' => 'required|string|max:255',
         ]);
 
